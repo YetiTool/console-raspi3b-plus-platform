a0b9bdd4c2a47cae7a45ce2061a8cc34ee21d25b
Klazomenai <19855141+Klazomenai@users.noreply.github.com>
2019-06-06 23:39:46 +0100

    Extending support scripts and fixing typo

--- ansible/templates/ansible-start.sh
+++ ansible/templates/ansible-start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # CPU Serial == PI Serial
-PI_SERIAL=`cat /proc/cpuinfo | grep Serial | awk ' {print $3}' | sed 's/^0*//'"`
+PI_SERIAL=`cat /proc/cpuinfo | grep Serial | awk ' {print $3}' | sed 's/^0*//'`
 
 ANSIBLE_CONFIG="/home/pi/console-raspi3b-plus-platform/ansible/ansible.cfg"
 ANSIBLE_INVENTORY="/home/pi/console-raspi3b-plus-platform/ansible/hosts"
@@ -10,5 +10,4 @@ ANSIBLE_LIBRARY="/home/pi/console-raspi3b-plus-platform/ansible/"
 /usr/bin/ansible-playbook \
 	    -e "serial=$PI_SERIAL" \
 	    -v \
-	    -l localhost \
 	    /home/pi/console-raspi3b-plus-platform/ansible/init.yaml
--- ansible/templates/support-stop.sh
+++ ansible/templates/support-stop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # CPU Serial
-BENCH=`cat /proc/cpuinfo | grep Serial | awk ' {print $3}'`
+BENCH=`cat /proc/cpuinfo | grep Serial | awk ' {print $3}' | sed 's/^0*//'`
 
 /usr/bin/pkill -f ${BENCH}
